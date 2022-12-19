fafe9a6d79236afcc34c10b733d1e99968fa2f26
Apple <apple@MacBook-Pro.local>
2022-12-19 23:53:28 +0530

    create vpc

--- environments/dev/outputs.tf
+++ environments/dev/outputs.tf
@@ -29,6 +29,7 @@ output "instance_name" {
   value = "${module.http_server.instance_name}"
 } */
 
-output "external_ip" {
+/* output "external_ip" {
   value = "${module.http_server.external_ip}"
 }
+ */
