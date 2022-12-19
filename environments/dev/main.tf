4e1507de6bfda2c8a43251529467252e74e2918c
Apple <apple@MacBook-Pro.local>
2022-12-19 22:07:41 +0530

    create vpc

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -27,7 +27,7 @@ module "vpc" {
   env     = "${local.env}"
 }
 
-module "http_server" {
+/* module "http_server" {
   source  = "../../modules/http_server"
   project = "${var.project}"
   subnet  = "${module.vpc.subnet}"
@@ -38,3 +38,4 @@ module "firewall" {
   project = "${var.project}"
   subnet  = "${module.vpc.subnet}"
 }
+ */
